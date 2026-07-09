5af0940f033ec3a39edcd4614d5b776beb009606
Jason Stefanou <sdi2200246@di.uoa.gr>
2026-07-09 14:44:19 +0300

    refact:Add application layer

---
+++ WorldRank/WorldRank.Application/Repositories/IPlayerRepository.cs
@@ -0,0 +1,17 @@
+﻿using WorldRank.Domain;
+
+namespace WorldRank.Application
+{
+	public interface IPlayerRepository
+	{
+		void AddPlayer(Player player);
+
+		IEnumerable<Player> GetAllPlayers();
+
+		void DeletePlayer(int playerId);
+
+		Player? FindPlayer(int playerId);
+
+		IEnumerable<IGrouping<int, Player>> GroupPlayersByScore();
+	}
+}
